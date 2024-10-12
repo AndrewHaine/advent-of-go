5db854dc97f0daca1ea904c77c1f46dbcca2915b
AndrewHaine <andrew@andrewhaine.co.uk>
2024-10-12 13:41:34 +0100

    🎅 2015 Day 1 Solutions

---
+++ 2015/01/solutions.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"andrewhaine/advent-of-go/util/aoc"
+	"strconv"
+
+	_ "embed"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	part := aoc.PartFlag()
+
+	if part == 1 {
+		aoc.PrintSolution(part1(input))
+	} else {
+		aoc.PrintSolution(part2(input))
+	}
+}
+
+func part1(partInput string) string {
+	instructions := aoc.ParseInput(partInput)
+
+	floor := 0
+
+	for _, instruction := range instructions {
+		if (instruction == '(') {
+			floor++
+		} else {
+			floor--
+		}
+	}
+
+	return strconv.Itoa(floor)
+}
+
+func part2(partInput string) string {
+	instructions := aoc.ParseInput(partInput)
+
+	var basementPos int
+	floor := 0
+
+	for i, instruction := range instructions {
+		if floor == -1 {
+			basementPos = i
+			break
+		}
+
+		if (instruction == '(') {
+			floor++
+		} else {
+			floor--
+		}
+	}
+
+	return strconv.Itoa(basementPos)
+}
